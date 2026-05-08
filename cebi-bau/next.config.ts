88282383a6741b43f4158def0763ae0d3960806c
EreDem <erendem.678@gmail.com>
2026-05-08 03:08:28 +0200

    config for github pages

--- cebi-bau/next.config.ts
+++ cebi-bau/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repoName = "cebi_bau";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
